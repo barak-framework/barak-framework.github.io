149d7017c503a08e7dbf13da9038d623e746ac38
Gökhan Demir <gdemir3327@gmail.com>
2019-03-04 20:44:55 +0300

    v4 to v5 converter shell

---
+++ guides/v5.0.0/v5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+filenames=$(grep -rl "redirect_to" app/controllers/*);
+for filename in $filenames; do
+sed -i 's/redirect_to("\//redirect_to("/g' $filename;
+done
+
+filename=config/routes.php;
+
+sed -i 's/get("\//get("/g' $filename;
+sed -i 's/post("\//post("/g' $filename;
+sed -i 's/scope("\//scope("/g' $filename;
+sed -i 's/resource("\//resource("/g' $filename;
+sed -i 's/resources("\//resources("/g' $filename;
+
+# filenames=$(grep -rl "render" *);
+# for filename in $filenames; do
+# sed -i 's/render("\//render("g' $filename;
+# done          
